3580ba7decbd2be28e4a73c55688689ac5098610
dlthaonhi <thaonhi6102005ll@gmail.com>
2026-02-06 10:10:40 +0700

    fix: save best checkpoint via ModelCheckpoint

--- src/piper/train/__main__.py
+++ src/piper/train/__main__.py
@@ -2,6 +2,7 @@ import logging
 
 import torch
 from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch.callbacks import ModelCheckpoint
 
 from .vits.dataset import VitsDataModule
 from .vits.lightning import VitsModel
@@ -26,8 +27,23 @@ def main():
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
     torch.backends.cudnn.deterministic = False
+    # CẤU HÌNH CHECKPOINT
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss",       # Theo dõi chỉ số val_loss được log trong lightning.py
+        mode="min",               # "min" nghĩa là càng thấp càng tốt
+        save_top_k=1,             # Chỉ lưu giữ 1 file tốt nhất
+        save_last=True,           # (Tùy chọn) Lưu thêm file cuối cùng (last.ckpt) để resume nếu cần
+        filename="best-epoch={epoch}-val_loss={val_loss:.4f}", # Đặt tên file để dễ nhận biết
+        auto_insert_metric_name=False
+    )
+
     _cli = VitsLightningCLI(  # noqa: ignore=F841
-        VitsModel, VitsDataModule, trainer_defaults={"max_epochs": -1}
+        VitsModel, 
+        VitsDataModule, 
+        trainer_defaults={
+            "max_epochs": -1, 
+            "callbacks": [checkpoint_callback] # callback
+        }
     )
 
 
